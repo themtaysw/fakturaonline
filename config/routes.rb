a2eb34af80bf6ff0f08c9c6216a997c8617579fe
Tonda Pleskac <pleskac@gmail.com>
2023-10-29 21:58:35 +0100

    Add form

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
-  post '/graphql', to: 'graphql#execute'
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+  end
+  post "/graphql", to: "graphql#execute"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   get :form, to: 'root#form'
 
-  root 'root#index'
+  # Defines the root path route ("/")
+  root "root#index"
 end
